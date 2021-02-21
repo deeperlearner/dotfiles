65165d99b50ea46cf07fc887505abad5852373f7
deeperlearner <b04202035@g.ntu.edu.tw>
2021-02-21 11:05:17 +0800

    .

--- append.sh
+++ append.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for FILE in '.bashrc' '.profile'; do
+for FILE in '.profile'; do
     if [ -e "$HOME/$FILE" ]; then
         if grep -q '^source ~/.append' "$HOME/$FILE"; then
             echo 'source has been added.'
