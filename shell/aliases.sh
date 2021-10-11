da41b7a6adf47bf0cb2654b881e55d0cbbf9764a
deeperlearner <b04202035@g.ntu.edu.tw>
2021-10-11 11:42:37 +0800

    Add cloc_git

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,6 +6,7 @@ alias watch_nv='watch -n 1 nvidia-smi'
 alias view_history='less /var/log/apt/history.log'
 alias rr='. ranger'
 alias lh='ls -lh'
+alias cloc_git='cloc --vcs=git --by-file'
 
 # show each submodules ahead or behind commits
 gss_command='git -c color.status=always status -s | \
